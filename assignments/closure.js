11e8a3152a7ec305ffaf7f1a597115ccca06338a
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-06-11 17:12:25 -0600

    Finished all MVP and Stretch goals for closures.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -16,26 +16,49 @@ function hello() {
 const counter = () => {
   let count = 0;
   return function() {
-    count = count + 1;
-    return count;
+    // count = count + 1; > this is the same as bellow
+    return ++count;
   }
 };
 
 const newCounter = counter();
-console.log(newCounter());
-console.log(newCounter());
-console.log(newCounter());
-console.log(newCounter());
-console.log(newCounter());
-console.log(newCounter());
+console.log(newCounter());// 1
+console.log(newCounter());// 1
+console.log(newCounter());// 1
+console.log(newCounter());// 1
+console.log(newCounter());// 1
+console.log(newCounter());// 1
 
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it."return ++count"
+// `decrement` should decrement the counter variable and return it. "return --count"
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment: () => (++count),
+    decrement: () => (--count)
+  }
 };
+
+const newCounterFactory = counterFactory();
+newCounterFactory.increment();// 1
+newCounterFactory.increment();// 2
+newCounterFactory.increment();// 3
+newCounterFactory.increment();// 4
+newCounterFactory.increment();// 5
+newCounterFactory.increment();// 6
+newCounterFactory.increment();// 7
+newCounterFactory.increment();// 8
+newCounterFactory.increment();// 9
+newCounterFactory.increment();// 10
+newCounterFactory.increment();// 11
+newCounterFactory.decrement();// 10
+newCounterFactory.decrement();// 9
+// The above newCounterFactory variable has ran, but we did not print our the results until the last one below
+console.log(newCounterFactory.increment());// 10
+
