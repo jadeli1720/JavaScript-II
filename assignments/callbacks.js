fe0d9c57f5fcf4853444989b99f7012530c57193
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-06-11 21:34:28 -0600

    Finished final Stretch goal from callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -42,54 +42,98 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum' , 'yo-yo'];
 */
 // The below function will be used in all the problems bellow to show the results of the functions I create
 function showResult(result) {
-  console.log('result:', result)
+  console.log('result:', result);
 }
 
 // 1
 function getLength(arr, cb) {
+  // getLength passes the length of the array into the callback.
   return cb(arr.length);
 }
 
 // Ivocation # 1:
-getLength(items, showResult);  
+getLength(items, showResult); //Should be 5 
 
 // Invocation # 2:
 // getLength( items, function(length) {
 //   console.log(length)
 // });
 
+
 // 2
 function last(arr, cb) {
+  // last passes the last item of the array into the callback.
   return cb(arr[3]);
 }
 
-last(items, function(last) {
-  console.log(last)
-});
+// Ivocation # 1:
+last(items, showResult);//Should be gum
+
+// // Invocation # 2
+// last(items, function(last) {
+//   console.log(last);
+// });
+
+
 
 // 3
 function sumNums(x, y, cb) {
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x + y);
 }
-
+// Invocation # 1
 sumNums(10, 10, showResult);
+
+// // Invocation # 2
+// sumNums(10, 15, function(total) {
+//   console.log(total);
+// });
+
 
 // 4
 function multiplyNums(x, y, cb) {
+  // multiplyNums multiplies two numbers and passes the result to the callback.
   return  cb(x * y);
 }
-
+// Invocation # 1
 multiplyNums(30, 10, showResult);
 
+// Invocation # 2:
+// multiplyNums(10, 15, function(total) {
+//   console.log(total);
+// });
 
+
+// 5
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false. arr.includes
- return cb(list.includes(item))
+ return cb(list.includes(item));
 }
 
 contains('Gum', items, showResult);
 contains('turd', items, showResult);
+
+// Second possible solution
+
+// function contains(item, list, cb) {
+//   // contains checks if an item is present inside of the given array/list.
+//   // Pass true to the callback if it is, otherwise pass false.
+//   for (let i = 0; i < list.length; i++) {
+//     if (list[i] === item) {
+//       return cb(true);
+//     }
+//   }
+//   return cb(false);
+// }
+
+// contains('Bats', items, function(flag) {
+//   console.log(flag); // should be false
+// });
+
+// contains('Notebook', items, function(flag) {
+//   console.log(flag); // should be true
+// });
 
 
 /* STRETCH PROBLEM */
@@ -98,4 +142,44 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let unique = array.filter(function (items, pos) {
+    //validates whether the first occurrence of current item in array
+   // equals the current position of the item (only return those items) 
+    return array.indexOf(items) == pos;
+  })
+  //wraps the result and passes it to cb function 
+  cb(unique)
 }
+
+
+// ***** Solution 2:
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+//   let unique = {};
+//     array.forEach(function (items) {
+//       if (!unique[items]) {
+//         unique[items] = true;
+//       }
+//     })
+//   //wraps the result and passes it to cb function 
+//   cb(unique)
+// } //works but prints out: result: { Pencil: true, Notebook: true, 'yo-yo': true, Gum: true } a little messy
+
+removeDuplicates(items, showResult);
+
+// **** Solution 3:
+function removeDuplicates(arr, cb) {
+  const seen = {};// a map of the keys. This will hold the values as a bool if 'seen' in the given array.
+  for (let i = 0; i < arr.length; i++) { // loop over array list and assign the object a 'key' of the given item w/ a value of 'true'
+    seen[arr[i]] = true; // this will set the key/value pair of each item found in the array to `true`. 
+    // It will not duplicate the item because Objects can only have one key of of each 'like' so. `yo-yo` will only be added 1 time etc. 
+  }
+  cb(Object.keys(seen));// Object.keys generates a list as an array of each key in the object passed to it.
+  // Could do the same of each value with `Object.values` :) 
+}
+
+removeDuplicates(items, function(dupeFreeArray) { 
+  console.log(dupeFreeArray);
+});
